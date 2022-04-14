9628971c5425fac6ed77b58dc5855a1c1a1baf43
Sandro Senna dos Anjos <sandro.sennaanjos@gmail.com>
2022-04-13 22:16:32 -0300

    Estacionamento
    Problema Solução Estacionamento

---
+++ exercicioEstacionamento.js
@@ -0,0 +1,44 @@
+function calcularNumeroDeEntradas(placa){
+    var numeroEntradas = 0
+  for(var i = 0; i < placas.length; i++){
+    if (placas[i] == placa){
+        numeroEntradas++ 
+    }
+  }   
+  return numeroEntradas  
+}
+ 
+function calcularValorDevido(placa){
+   numeroEntradas = calcularNumeroDeEntradas(placa)
+    var valor = 0
+if(numeroEntradas <= 20){
+valor = numeroEntradas * 10
+} else{
+    valor = 20 * 10 + (5 *(numeroEntradas - 20))
+}
+return valor
+}
+
+var placas = [
+    'RXB-2525', 'AKX-3333', 'ORO-7142', 'RXB-2525',
+    'AKX-3333', 'ORO-7142', 'AKX-3333', 'RXB-2525',
+    'AKX-3333', 'AKX-3333', 'RXB-2525', 'AKX-3333',
+    'RXB-2525', 'AKX-3333', 'ORO-7142', 'AKX-3333',
+    'AKX-3333', 'RXB-2525', 'AKX-3333', 'ORO-7142',
+    'ORO-7142', 'AKX-3333', 'AKX-3333', 'RXB-2525',
+    'AKX-3333', 'AKX-3333', 'RXB-2525', 'AKX-3333',
+    'AKX-3333', 'RXB-2525', 'AKX-3333', 'ORO-7142',
+    'ORO-7142', 'AKX-3333', 'ORO-7142', 'ORO-7142',
+    'ORO-7142', 'RXB-2525', 'AKX-3333', 'AKX-3333',
+    'ORO-7142', 'ORO-7142', 'AKX-3333', 'RXB-2525',
+    'AKX-3333', 'AKX-3333', 'RXB-2525', 'AKX-3333',
+    'RXB-2525', 'AKX-3333', 'ORO-7142', 'AKX-3333',
+    'AKX-3333', 'RXB-2525', 'AKX-3333', 'ORO-7142',
+    'ORO-7142', 'AKX-3333', 'AKX-3333', 'RXB-2525',
+    'AKX-3333', 'AKX-3333', 'RXB-2525', 'AKX-3333',
+    'AKX-3333', 'RXB-2525'
+ ]
+ 
+
+console.log(calcularNumeroDeEntradas("AKX-3333") + ' entradas')
+console.log('R$' + calcularValorDevido('AKX-3333'))
